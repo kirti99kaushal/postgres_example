f812ef4782c6406ac0cf2e0456bcb49950be9b0d
Kirti Kaushal <kaushalkirti322@gmail.com>
2019-04-12 15:05:47 +0530

    a

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, jsonify, render_template
+from flask import Flask,redirect, url_for, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from sqlalchemy import extract
