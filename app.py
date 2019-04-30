73225a91158609f0abc10517f8b723f879f75ec5
Kirti Kaushal <kaushalkirti322@gmail.com>
2019-04-30 21:08:55 +0530

    h

--- app.py
+++ app.py
@@ -627,7 +627,7 @@ def getevents():
         Result=''
         response=''
         reply= ''
-        for row in events:
+        for row in event:
 
             i = i + 1
             print("print rows", row.id, row.start_date, row.end_date, row.event)
