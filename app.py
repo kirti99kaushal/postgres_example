374de28d9cf189cc8124d4eb081bab4d6845786c
Kirti Kaushal <kaushalkirti322@gmail.com>
2019-05-01 21:15:08 +0530

    h

--- app.py
+++ app.py
@@ -250,7 +250,7 @@ def schedule():
                 
             reply = {"fulfillmentText": response,}
 
-        return jsonify(reply)
+            return jsonify(reply)
         
     except Exception as e:
         return(str(e))
