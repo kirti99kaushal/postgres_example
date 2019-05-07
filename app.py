99617acc304f226d608264393ff9908adea84e3e
Kirti Kaushal <kaushalkirti322@gmail.com>
2019-05-07 15:40:40 +0530

    s

--- app.py
+++ app.py
@@ -783,7 +783,8 @@ def assistant():
 
 def find_song_and_play(req):
         song_name = req.get("queryResult").get("parameters").get("songname")
-        song_url = search_song_url(song_name)
+        #song_url = search_song_url(song_name)
+        song_url = "https://api.anyaudio.in/api/v1/search?q="
         reply=""
         my_result = {
             "fulfillmentText": "This is a text response",
